46b24f01f220d24d5a6861255075248aa4e4a379
Roman <roman.stupko@gmail.com>
2024-05-03 18:26:14 +0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import "@fontsource/inter";
+import { CssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
+import Sidebar from "./components/Sidebar";
+import OrderTable from "./components/OrderTable";
+import OrderList from "./components/OrderList";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <CssVarsProvider disableTransitionOnChange>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+        <Header />
+        <Sidebar />
+        <Box
+          component="main"
+          className="MainContent"
+          sx={{
+            px: { xs: 2, md: 6 },
+            pt: {
+              xs: 'calc(12px + var(--Header-height))',
+              sm: 'calc(12px + var(--Header-height))',
+              md: 3,
+            },
+            pb: { xs: 2, sm: 2, md: 3 },
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            minWidth: 0,
+            height: '100dvh',
+            gap: 1,
+          }}
+        >
+
+          <Box
+            sx={{
+              display: 'flex',
+              mb: 1,
+              gap: 1,
+              flexDirection: { xs: 'column', sm: 'row' },
+              alignItems: { xs: 'start', sm: 'center' },
+              flexWrap: 'wrap',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography level="h2" component="h1">
+              Active tasks
+            </Typography>
+          </Box>
+          <OrderTable />
+          <OrderList />
+        </Box>
+      </Box>
+    </CssVarsProvider>
+  );
+}
+
+export default App
